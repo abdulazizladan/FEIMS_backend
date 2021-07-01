b49e56dc2ffbfa987fdcf0b7b8c02416f9b3eae8
Alhassan Ibrahim <zerosoftwere@gmail.com>
2021-07-01 11:40:54 +0100

    Fix: Add cors middleware

--- application.js
+++ application.js
@@ -2,10 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const cors = require("cors");
 
 const application = express();
 application.use(morgan('dev'));
 application.use(bodyParser.json());
+application.use(cors());
+
 routes.intialize(application);
 
 application.get('/', (req, res) => {
